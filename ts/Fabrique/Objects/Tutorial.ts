37a616439e916f1e391553126606162aff8a6240
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2020-02-11 14:59:02 +0300

    Изменения в объекте Tutorial

--- ts/Fabrique/Objects/Tutorial.ts
+++ ts/Fabrique/Objects/Tutorial.ts
@@ -23,7 +23,7 @@ module Fabrique {
             graphics.endFill();
 
             graphics.beginFill(0x000000, 0.5);
-            graphics.lineStyle(2, 0xFFFFFF, 0.5);
+            graphics.lineStyle(2, 0x999999, 0.5);
             graphics.drawRect(0,0,400,116);
             graphics.endFill();
 
