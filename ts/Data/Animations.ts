a94a97847c346a9a15edf820f052ce3f2ef392cd
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2020-02-20 09:37:50 +0300

    Подключены атласы

--- ts/Data/Animations.ts
+++ ts/Data/Animations.ts
@@ -1,7 +1,33 @@
-class Animations { 
-    public static Liukang: string = 'liukang.json';
-    
+class Jsons { 
+    public static BarakaJson: string = 'baraka.json';
+    public static GoroJson: string = 'goro.json';
+    public static JaxJson: string = 'jax.json';
+    public static JohnnyCageJson: string = 'johnnycage.json';
+    public static KitanaJson: string = 'kitana.json';
+    public static KungLaoJson: string = 'kunglao.json';
+    public static LiuKangJson: string = 'liukang.json';
+    public static MileenaJson: string = 'mileena.json';
+    public static RaidenJson: string = 'raiden.json';
+    public static ReptileJson: string = 'reptile.json';
+    public static ScorpionJson: string = 'scorpion.json';
+    public static ShangtsungJson: string = 'shangtsung.json';
+    public static ShaokahnJson: string = 'shaokahn.json';
+    public static SubzeroJson: string = 'subzero.json';
+
     public static preloadList:Array<string> = [
-        Animations.Liukang,
+        Jsons.BarakaJson,
+        Jsons.GoroJson,
+        Jsons.JaxJson,
+        Jsons.JohnnyCageJson,
+        Jsons.KitanaJson,
+        Jsons.KungLaoJson,
+        Jsons.LiuKangJson,
+        Jsons.MileenaJson,
+        Jsons.RaidenJson,
+        Jsons.ReptileJson,
+        Jsons.ScorpionJson,
+        Jsons.ShangtsungJson,
+        Jsons.ShaokahnJson,
+        Jsons.SubzeroJson
     ];
 }
--- ts/Data/Atlases.ts
+++ ts/Data/Atlases.ts
@@ -4,13 +4,45 @@ class Atlases {
     public static Video2: string = 'video2';
     public static Video3: string = 'video3';
     public static VideoHelp: string = 'video_help';
-    public static LiukangAnimation: string = 'liukang';
+
+    public static BarakaAnimation: string = 'baraka';
+    public static GoroAnimation: string = 'goro';
+    public static JaxAnimation: string = 'jax';
+    public static JohnnyCageAnimation: string = 'johnnycage';
+    public static KitanaAnimation: string = 'kitana';
+    public static KungLaoAnimation: string = 'kunglao';
+    public static LiuKangAnimation: string = 'liukang';
+    public static MileenaAnimation: string = 'mileena';
+    public static RaidenAnimation: string = 'raiden';
+    public static ReptileAnimation: string = 'reptile';
+    public static ScorpionAnimation: string = 'scorpion';
+    public static ShangtsungAnimation: string = 'shangtsung';
+    public static ShaokahnAnimation: string = 'shaokahn';
+    public static SubzeroAnimation: string = 'subzero';
+
+    public static Blood: string = 'blood';
+    public static Flash: string = 'flash';
     
     public static preloadList:Array<string> = [
         Atlases.Video1,
         Atlases.Video2,
         Atlases.Video3,
         Atlases.VideoHelp,
-        Atlases.LiukangAnimation
+        Atlases.Blood,
+        Atlases.Flash,
+        Atlases.BarakaAnimation,
+        Atlases.GoroAnimation,
+        Atlases.JaxAnimation,
+        Atlases.JohnnyCageAnimation,
+        Atlases.KitanaAnimation,
+        Atlases.KungLaoAnimation,
+        Atlases.LiuKangAnimation,
+        Atlases.MileenaAnimation,
+        Atlases.RaidenAnimation,
+        Atlases.ReptileAnimation,
+        Atlases.ScorpionAnimation,
+        Atlases.ShangtsungAnimation,
+        Atlases.ShaokahnAnimation,
+        Atlases.SubzeroAnimation
     ];
 }
