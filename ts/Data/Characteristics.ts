0a7b7f7e7707eb7b8860050b81a78ef344036a84
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2020-02-20 10:19:48 +0300

    Характеристики персонажей

--- ts/Data/Characteristics.ts
+++ ts/Data/Characteristics.ts
@@ -1,5 +1,21 @@
 class Characteristics {
 
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+    public static a: string = '_json.json';
+
+
     public static liukangJson: string = 'liukang_json.json';
 
     public static preloadList:Array<string> = [
